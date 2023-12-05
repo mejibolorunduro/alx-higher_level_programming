d7daed9050a9de59c66975bf8865c9698d885c34
mejibolorunduro <davidbolorunduro2004@gmail.com>
2023-12-05 09:41:40 +0100

    0x03-python-data_structures

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     for name in sorted(dir(hidden_4)):
         # print all and only names that do not start with __
         if name[:2] != '__':
-            print("{}".format(name)
+            print("{}".format(name))
