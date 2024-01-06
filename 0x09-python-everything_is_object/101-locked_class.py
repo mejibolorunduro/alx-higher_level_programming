f8cd4b9a2de2d952a0f92be778cf5acbccf0f6ca
mejibolorunduro <davidbolorunduro2004@gmail.com>
2024-01-05 20:52:47 -0800

    0x09-python

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,8 +5,10 @@
 
 
 class LockedClass:
-    """Prevent the user from instantiating new LockedClass attributes
-    for anything but attributes called 'first_name'."""
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
 
 
     __slots__ = ["first_name"]
